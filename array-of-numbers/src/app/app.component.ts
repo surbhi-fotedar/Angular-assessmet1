d6fc9368a5e265e2a047198290b4b21664d3a5ce
Surbhi Fotedar <surbhif@CESCHNDES-0108.CESCHN.COM>
2018-11-07 16:30:47 +0530

    onClickMe is returning the required pair of numbers along with index

--- array-of-numbers/src/app/app.component.ts
+++ array-of-numbers/src/app/app.component.ts
@@ -10,26 +10,35 @@ export class AppComponent{
 
   ngOnInit () {  }
   
-  intnum: number[] = [2,4,1,5,9,13,12,3,8];
-  add: number = 12;
-  firstvalue: number;
-  Secondvalue: number;
-
+  strnum: number[];
+  intnum: string[];
+  add: number;
+  firstvalue: string;
+  Secondvalue: string;
+  firstIndex: number;
+  secondIndex: number;
+  
+  
   onClickMe() {
+        
+    this.intnum = this.strnum.toString().split(',');
+   for(let i: number = 0; i < this.strnum.toString().split(',').length; i++) {
     
-   for(let i: number = 0; i < this.intnum.length; i++) {
-    for(let j = i+1; j < this.intnum.length; j++) {
-      if(this.intnum[i] + this.intnum[j] == 12) {
+    for(let j = i+1; j < this.strnum.toString().split(',').length; j++) {
+      
+       if(Number(this.intnum[i]) + Number(this.intnum[j]) === Number(this.add)) {
         console.log('pair found ' + this.intnum[i] +' ' +this.intnum[j] +' indexes '+ i +' '+j );
 
         this.firstvalue = this.intnum[i];
+        this.firstIndex = i;
         this.Secondvalue = this.intnum[j];
+        this.secondIndex = j;
       }
       else {
         console.log('no pair');
       }
     }  
-      
+      //  2,4,1,5,9,13,12,3,8 
       
     }
 
